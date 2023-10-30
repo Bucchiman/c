5604eaf4690e6f9f207fea262002285aa7f9d9dd
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-30 18:35:44 +0900

    update Bmods library

---
+++ pocket/lib/Bmods.c
@@ -0,0 +1,16 @@
+/*
+ * FileName:     Bmods
+ * Author:       8ucchiman
+ * CreatedDate:  2023-10-30 12:52:37
+ * LastModified: 2023-02-25 18:37:47 +0900
+ * Reference:    8ucchiman.jp
+ */
+
+
+
+#include <stdio.h>
+#include "Bmods.h"
+
+void print_8ucchiman (void) {
+    printf("8ucchiman was here!!!\n");
+}
---
+++ pocket/lib/Bmods.h
@@ -0,0 +1,3 @@
+
+
+void print_8ucchiman (void);
